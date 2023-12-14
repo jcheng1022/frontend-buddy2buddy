537b6fd41086d383f272b5807838236f1557a411
jcheng1022 <jackycheng1022@gmail.com>
2023-12-14 10:25:31 -0500

    update??

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -47,12 +47,22 @@ const SignUp = () => {
     }
 
     const handleResend = async () => {
+        const getURL = () => {
+            let url =
+                process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+                process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+                'http://localhost:3000/'
+            // Make sure to include `https://` when not localhost.
+            url = url.includes('http') ? url : `https://${url}`
+            // Make sure to include a trailing `/`.
+            url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+            return url
+        }
         const {data,error} = await supabase.auth.resend({
             type: 'signup',
             email: loginForm.email,
             options: {
-                emailRedirectTo: 'https://frontend-productivity-app.vercel.app/'
-                // emailRedirectTo: 'http://localhost:3000/'
+                emailRedirectTo: getURL()
             }
         })
 
